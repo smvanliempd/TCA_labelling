7be210c28e67ef87f45cb9fbadea5f4e6b42dd51
smvanliempd <smvanliempd@gmail.com>
2023-06-23 11:37:40 +0200

    convert from list-based to tidyverse

--- cycle_funs.R
+++ cycle_funs.R
@@ -1,69 +1,110 @@
-# cycle functions
-filter_dups <- function(ent_fwd, ent_rev, e) {
-  if (all(ent_fwd == ent_rev)) {
-    ent <- ent_fwd
-    ent_reg <- c(sum(ent), e$reg[2], strtoi(paste0(ent_fwd,collapse = ""), base = 2))
-    return(list(list(ent = ent,
-                     reg = ent_reg)))
-  } else {
-    ent_fwd_reg <- c(sum(ent_fwd), e$reg[2]/2, strtoi(paste0(ent_fwd,collapse = ""), base = 2) )
-    ent_rev_reg <- c(sum(ent_rev), e$reg[2]/2, strtoi(paste0(ent_rev,collapse = ""), base = 2) )
-    return(list(list(ent = ent_fwd,
-                     reg = ent_fwd_reg),
-                list(ent = ent_rev,
-                     reg = ent_rev_reg ) ))
-  }
-} 
-icit_to_akg <- function(icit) {
-  akg <- sapply(icit, function(i) {
-    akg_fwd <- i$ent[1:5]
-    akg_rev <- i$ent[5:1]
-    filter_dups(akg_fwd,akg_rev,i)
-  }, simplify = FALSE )
-  akg <- unlist(akg,recursive = FALSE)
+clean_isos <- function(l) {
+  x <- do.call(rbind, l) |>
+    data.frame() |>
+    group_by(X3) |>
+    mutate(X2 = sum(X2)) |>
+    distinct() |>
+    ungroup()
+  return(x)
 }
 akg_to_suc <- function(akg) {
-  suc <- sapply(akg, function(a) {
-    suc <- a$ent[2:5]
-    suc_reg <- c(sum(suc), a$reg[2],strtoi(paste0(suc,collapse = ""), base = 2)  )
-    return(list(ent = suc,
-                reg = suc_reg))
-  },simplify = FALSE )
-} 
+  suc <- akg
+  suc[,4] <- NA
+  suc <- apply(suc, 1, function(s) {
+    s[1] <- sum(s[5:8])
+    s[3] <- strtoi(paste0(s[5:8],collapse = ""), base = 2)
+    return(s)
+  }, simplify = FALSE)
+  suc <- clean_isos(suc)
+  return(suc)
+}
 suc_to_mal <- function(suc) {
-  mal <- sapply(suc, function(s) {
-    mal_fwd <- s$ent[1:4]
-    mal_rev <- s$ent[4:1]
-    filter_dups(mal_fwd,mal_rev,s)
-  }, simplify = FALSE )
-  mal <- unlist(mal,recursive = FALSE)
-}
-mal_to_icit <- function(mal, accoa = c(0,0) ) {
-  icit <- sapply(mal, function(m) {
-    icit <- c(accoa, m$ent[2:4], m$ent[1])
-    icit_reg <- c(sum(icit),m$reg[2],strtoi(paste0(icit,collapse = ""), base = 2) )
-    return(list(ent = icit,
-                reg = icit_reg))
-  },simplify = FALSE)
-}
+  mal <- apply(suc, 1, function(s) {
 
-# recursive cycle function
-cycle <- function(n, akg_init) {
+    # forward seq
+    mal_fwd <- s
+    mal_fwd[2] <- mal_fwd[2]/2
+    names(mal_fwd) <- paste0("X",1:9)
+    
+    # reverse seq
+    mal_rev <- s[c(1:3,9:4)]
+    mal_rev[2] <- mal_rev[2]/2
+    mal_rev[3] <- strtoi(paste0(mal_rev[5:8],collapse = ""), base = 2)
+    names(mal_rev) <- paste0("X",1:9)
+    
+    # return
+    rbind(mal_fwd,mal_rev)
+    
+  }, simplify = FALSE )
+  mal <- clean_isos(mal)
+  return(mal)
+}
+mal_to_icit <- function(mal, ac = c(0,0)) {
+  icit <- apply(mal, 1, function(m) {
+
+    # forward seq
+    icit_fwd <- m[c(1:4,9,6:8,5)] # mov
+    icit_fwd[4:5] <- ac           # add
+    icit_fwd[2] <- icit_fwd[2]/2
+    icit_fwd[3] <- strtoi(paste0(icit_fwd[4:9],collapse = ""), base = 2)
+    names(icit_fwd) <- paste0("X",1:9)
+    
+    # reverse seq
+    icit_rev <- m[c(1:4,9,7:5,8)] # mov
+    icit_rev[4:5] <- ac           # add
+    icit_rev[2] <- icit_rev[2]/2
+    icit_rev[3] <- strtoi(paste0(icit_rev[4:9],collapse = ""), base = 2)
+    names(icit_fwd) <- paste0("X",1:9)
+    
+    # return
+    rbind(icit_fwd,icit_rev)
+    
+  }, simplify = FALSE )
+  icit <- clean_isos(icit)
+  return(icit)
+}
+icit_to_akg <- function(icit) {
+  akg <- apply(icit, 1, function(i) {
+    akg <- i
+    akg[9] <- NA
+    akg[1] <- sum(akg[4:8])
+    akg[3] <- strtoi(paste0(akg[4:8],collapse = ""), base = 2)
+    return(akg)
+  },simplify = FALSE)
+  akg <- clean_isos(akg)
+  return(akg)
+}
+inject_akg <- function(akg_cyc, akg_inj, p_inj) {
+  akg_cyc <- akg_cyc|>
+    mutate(X2 = X2 * (1-p_inj) )
   
-  if (n == N_cycle) {
-    cat("stop")
+  akg_inj <- apply(akg_inj, 1, function(a) {
+    a[2] <- a[2] * p_inj
+    a[3] <- strtoi(paste0(a[4:8],collapse = ""), base = 2)
+    return(a)
+  },simplify = FALSE)
+  akg_inj <- clean_isos(akg_inj)
+  akg <- rbind(akg_cyc,akg_inj) 
+  return(akg)
+}
+cycle_rec <- function(n = 1, akg_init, akg_cyc, L = list(), p_inj ) {
+  if(n == N_cycle + 1) {
+    cat("done")
+    return(L)
   } else {
-    suc  <- akg_to_suc(akg = akg_init)
-    mal  <- suc_to_mal(suc = suc)
-    icit <- mal_to_icit(mal = mal)
-    akg  <- icit_to_akg(icit = icit)
-    
-    # TODO clean up dups for akg
-    
-    L[[n]] <<- list(suc = suc,mal = mal,icit =  icit,akg =akg)
-    cycle(n + 1, akg)
+    suc <- akg_to_suc(akg_cyc)
+    mal <- suc_to_mal(suc)
+    icit <- mal_to_icit(mal)
+    akg <- icit_to_akg(icit)
+    akg_fin <- inject_akg(akg_cyc = akg,
+                          akg_inj = akg_init, 
+                          p_inj   = p_inj)
+    L[[n]] <- list(suc  = suc,
+                   mal  = mal,
+                   icit = icit,
+                   akg  = akg_fin)
+    cycle_rec(n = n + 1, akg_init, akg_fin, L = L, p_inj = p_inj)
   }
-  
 }
 
 
--- test.R
+++ test.R
@@ -1,21 +1,13 @@
-icit_init <- list(list(ent = c(0,0,1,1,1,1),
-                       reg = c(4,0.5,1)),
-                  list(ent = c(0,0,0,0,0,0),
-                       reg = c(0,0.5,1)))
-akg_init <- icit_to_akg(icit_init)
-suc_init <- akg_to_suc(akg_init)
-mal_init <- suc_to_mal(suc_init)
-icit_fin <- mal_to_icit(mal_init)
+library(dplyr)
 
-# 2nd cycle
-akg_2nd  <- icit_to_akg(icit_fin)
-suc_2nd <- akg_to_suc(akg_2nd)
-mal_2nd <- suc_to_mal(suc_2nd)
-
-# cycle
+akg_00 <- rbind(
+  # c(5,0.025, NA,1,1,1,1,0,NA),
+  # c(4,0.025,NA,0,1,1,1,1,NA),
+  c(5,1,NA,1,1,1,1,1,NA)) |>
+  data.frame( ) |>
+  tibble()
+prop_inj <- 0.9
 N_cycle <- 10
-L <- list()
-akg_init <- list(list(ent = c(1,1,1,1,1),
-                      reg = c(5,1,1)))
-cycle(n =1 , akg_init = akg_init )
 
+isotopomers <- cycle_rec(akg_init = akg_00, akg_cyc = akg_00,p_inj = prop_inj)
+isotopomers
