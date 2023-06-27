93070cf73e7d3aef9b0daaba9d6d9b3b78b944c9
smvanliempd <smvanliempd@gmail.com>
2023-06-27 11:12:45 +0200

    added vars

---
+++ variables.R
@@ -0,0 +1,19 @@
+# labelled via 13C5-glutamine
+akg_00 <- rbind(
+  # c(5,0.025, NA,1,1,1,1,0,NA),
+  # c(4,0.025,NA,0,1,1,1,1,NA),
+  c(5,1,NA,1,1,1,1,1,NA)
+) |>
+  data.frame( ) |>
+  tibble()
+AcCoA <- c(0,0)
+prop_inj <- 0.3
+N_cycle <- 15
+
+# labelled AcCoA via glucose
+akg_00 <- rbind(c(0,1,NA,0,0,0,0,0,NA) ) |>
+  data.frame() |>
+  tibble()
+AcCoA <- c(1,1)
+prop_inj <- 0.1
+N_cycle <- 20
